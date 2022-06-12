a5920d5ad356f955a68ae4ce15b8d85739dc2530
Steven <stevenrojascubero@gmail.com>
2022-06-11 18:15:10 -0600

    Added flag option -t to run tests

--- build.sh
+++ build.sh
@@ -7,4 +7,12 @@ set -e
     cd build
     cmake ..
     cmake --build .
+    while getopts "t" flags; do
+        case $flags in
+            t)
+                printf "\n\nRunning tests...\n\n"
+                ../build/test/test
+                ;;
+        esac
+    done
 )
